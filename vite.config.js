f4b06fd3dc7eecb46d7d4c81e5daa06a024efcc5
Danson <githukadanson23@gmail.com>
2025-06-16 22:01:27 +0300

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<React-portfolio >/', // e.g. '/my-portfolio/'
+  base: '/React-portfolio /', // e.g. '/my-portfolio/'
   plugins: [react()],
 });
